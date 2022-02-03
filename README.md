bb1a6cfff2658a2d6a5c627e93d140931e6b98a3
GeorgeGedox <39589642+GeorgeGedox@users.noreply.github.com>
2022-02-03 16:13:11 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# QMK Keymap
+
+## Target: GMMK Pro ANSI
+
+### How to use
+1. Setup QMK environment as documented in [here](https://docs.qmk.fm/#/newbs_getting_started)
+2. Clone this repo in `keyboards/gmmk/pro/ansi/keymaps`
+3. Compile with `qmk compile -kb gmmk/pro/ansi -km <name-of-the-cloned-repo>`
+4. Flash the built firmware either via CLI or with [QMK Toolbox](https://github.com/qmk/qmk_toolbox)
---
+++ keymap.c
@@ -0,0 +1,144 @@
+/* Copyright 2022 GeorgeGedox <https://github.com/GeorgeGedox>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include QMK_KEYBOARD_H
+
+// RGB LED layout
+
+// led number, function of the key
+
+//  67, Side led 01    0, ESC      6, F1       12, F2       18, F3       23, F4       28, F5       34, F6       39, F7       44, F8       50, F9       56, F10      61, F11      66, F12      69, Prt       Rotary(Mute)   68, Side led 12
+//  70, Side led 02    1, ~        7, 1        13, 2        19, 3        24, 4        29, 5        35, 6        40, 7        45, 8        51, 9        57, 0        62, -_       78, (=+)     85, BackSpc   72, Del        71, Side led 13
+//  73, Side led 03    2, Tab      8, Q        14, W        20. E        25, R        30, T        36, Y        41, U        46, I        52, O        58, P        63, [{       89, ]}       93, \|        75, PgUp       74, Side led 14
+//  76, Side led 04    3, Caps     9, A        15, S        21, D        26, F        31, G        37, H        42, J        47, K        53, L        59, ;:       64, '"                    96, Enter     86, PgDn       77, Side led 15
+//  80, Side led 05    4, Sh_L     10, Z       16, X        22, C        27, V        32, B        38, N        43, M        48, ,<       54, .<       60, /?                    90, Sh_R     94, Up        82, End        81, Side led 16
+//  83, Side led 06    5, Ct_L     11,Win_L    17, Alt_L                              33, SPACE                              49, Alt_R    55, FN                    65, Ct_R     95, Left     97, Down      79, Right      84, Side led 17
+//  87, Side led 07                                                                                                                                                                                                        88, Side led 18
+//  91, Side led 08
+
+// clang-format off
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+//      ESC      F1       F2       F3       F4       F5       F6       F7       F8       F9       F10      F11      F12	     Prt           Rotary(Mute)
+//      ~        1        2        3        4        5        6        7        8        9        0         -       (=)	     BackSpc           Del
+//      Tab      Q        W        E        R        T        Y        U        I        O        P        [        ]        \                 PgUp
+//      Caps     A        S        D        F        G        H        J        K        L        ;        "                 Enter             PgDn
+//      Sh_L              Z        X        C        V        B        N        M        ,        .        ?                 Sh_R     Up       End
+//      Ct_L     Win_L    Alt_L                               SPACE                               Alt_R    FN       Ct_R     Left     Down     Right
+
+    [0] = LAYOUT(
+        KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,           KC_MPLY,
+        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,          KC_PGUP,
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,          KC_PGDN,
+        KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,           KC_HOME,
+        KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT, KC_UP,   KC_END,
+        KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, MO(1),   KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
+    ),
+
+    [1] = LAYOUT(
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_MPRV, _______, _______, KC_MNXT, RESET,            _______,
+        _______, RGB_TOG, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          RGB_HUI,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          RGB_HUD,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,          _______,
+        _______,          _______, _______, _______, _______, _______, NK_TOGG, _______, _______, _______, _______,          _______, RGB_MOD, _______,
+        _______, _______, _______,                            _______,                            _______, _______, _______, RGB_SPD, RGB_RMOD, RGB_SPI
+    ),
+
+
+};
+
+static void f_rows_set_rgb(uint8_t type, uint8_t R, uint8_t G, uint8_t B) {
+    switch (type) {
+        case 1: // Left
+            rgb_matrix_set_color(6, R, G, B);
+            rgb_matrix_set_color(12, R, G, B);
+            rgb_matrix_set_color(18, R, G, B);
+            rgb_matrix_set_color(23, R, G, B);
+            break;
+        case 2: // Middle
+            rgb_matrix_set_color(28, R, G, B);
+            rgb_matrix_set_color(34, R, G, B);
+            rgb_matrix_set_color(39, R, G, B);
+            rgb_matrix_set_color(44, R, G, B);
+            break;
+        case 3: // Right
+            rgb_matrix_set_color(50, R, G, B);
+            rgb_matrix_set_color(56, R, G, B);
+            rgb_matrix_set_color(61, R, G, B);
+            rgb_matrix_set_color(66, R, G, B);
+            break;
+        default:
+            f_rows_set_rgb(1, R, G, B);
+            f_rows_set_rgb(2, R, G, B);
+            f_rows_set_rgb(3, R, G, B);
+            break;
+    }
+}
+
+// Knob
+#if defined(ENCODER_ENABLE)
+bool encoder_update_user(uint8_t index, bool clockwise) {
+    uint8_t mod_state = get_mods();
+    if (get_highest_layer(layer_state|default_layer_state) > 0) {
+        if (clockwise) {
+            rgb_matrix_increase_val();
+        } else {
+            rgb_matrix_decrease_val();
+        }
+    } else {
+        if (mod_state & MOD_MASK_CTRL) {
+            unregister_mods(MOD_MASK_CTRL);
+            if (clockwise) {
+                tap_code16(LCTL(KC_RGHT));
+            } else {
+                tap_code16(LCTL(KC_LEFT));
+            }
+            set_mods(mod_state);
+        } else {
+            if (clockwise) {
+                tap_code16(KC_VOLU);
+            } else {
+                tap_code16(KC_VOLD);
+            }
+        }
+    }
+
+    return false;
+}
+#endif
+
+void rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
+
+    if (IS_HOST_LED_ON(USB_LED_CAPS_LOCK)) {
+        f_rows_set_rgb(1, RGB_RED);
+        rgb_matrix_set_color(3, RGB_RED);
+    }
+
+    // Highlight configured keys on next layer
+    if (get_highest_layer(layer_state) > 0) {
+        uint8_t layer = get_highest_layer(layer_state);
+
+        for (uint8_t row = 0; row < MATRIX_ROWS; ++row) {
+            for (uint8_t col = 0; col < MATRIX_COLS; ++col) {
+                uint8_t index = g_led_config.matrix_co[row][col];
+
+                if (index >= led_min && index <= led_max && index != NO_LED && keymap_key_to_keycode(layer, (keypos_t){col,row}) > KC_TRNS) {
+                    rgb_matrix_set_color(index, RGB_GREEN);
+                }
+            }
+        }
+    }
+}
